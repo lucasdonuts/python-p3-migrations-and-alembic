e88fa9025d9f37b1566eeea8bd6a8eee9369b3a4
Lucas Duncan <lucasdonuts@gmail.com>
2023-01-19 19:58:03 -0600

    Fin

---
+++ lib/migration/versions/b74add8fd4ef_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b74add8fd4ef
+Revises: 
+Create Date: 2023-01-19 19:45:25.329610
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b74add8fd4ef'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
